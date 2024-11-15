aeecfadc0f630b4890d46bdd58ab8f48f3cb9f07
siraajul <shirajulislamparvez@gmail.com>
2024-11-15 07:40:02 +0600

    Update: DebugBanner Removed

--- lib/app.dart
+++ lib/app.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
